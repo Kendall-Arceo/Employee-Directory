30208b7aded244391ad086414129e6895d4e6282
Kendall Arceo <arceokendall@gmail.com>
2020-05-24 22:31:06 -0500

    added components, jsx file for the employee table functionality, and a json file of dummy employees to have react appwork.

---
+++ employeedirectoryapp/src/components/EmployeeTable.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const EmployeeTable = props => {
+    console.log(props.search)
+    console.log(props.employeeList)
+    function filter(employee){
+        return(
+            employee.name.includes(props.search)
+        )
+    }
+
+    
+    const EmptyRows = () => {
+        return props.employeeList.filter(filter).map(emp=>{
+            return (
+                <tr key={emp.name}>
+                    <td>{emp.name}</td>
+                    <td>{emp.job}</td>
+                    <td>{emp.pay}</td>
+                </tr>
+            )
+        })
+    }
+    return (
+        <table className="table">
+            <thead className="tableHead">
+                <tr>
+                <td onClick={props.sortEmps}>name</td>
+                <td>position</td>
+                <td>pay</td>
+                </tr>
+            </thead>
+            <tbody>
+
+            { EmptyRows() }
+            </tbody>
+        </table>
+    )
+}
+
+export default EmployeeTable
